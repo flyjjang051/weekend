9b44f5cd3be8e533e01c8f4c0036b2fde0ef5d1a
jjang051 <flyjjang051@gmail.com>
2022-05-08 10:38:19 +0900

    result list 오나성

--- rock_paper_scissors/js/main.js
+++ rock_paper_scissors/js/main.js
@@ -1,6 +1,7 @@
 console.log("hello javascript");
 const humanItem = document.querySelectorAll("#human ul li");
 const comItem = document.querySelectorAll("#computer ul li");
+const resultList = document.querySelector("#resultList");
 console.log("🚀 ~ file: main.js ~ line 4 ~ comItem", comItem);
 console.log("🚀 ~ file: main.js ~ line 3 ~ humanItem", humanItem);
 
@@ -30,22 +31,31 @@ restart();
 
 //randomChoice();
 
+//
 for (let i = 0; i < 3; i++) {
   humanItem[i].addEventListener("click", function () {
     console.log("comChoiceNum===", comChoiceNum);
     clearInterval(comChoiceStop);
+    let item = null;
     if (i === comChoiceNum) {
       console.log("비겼습니다.");
+      item = `<li class="draw">D</li>`;
     } else if (i === 0 && comChoiceNum === 2) {
       console.log("이겼습니다.");
+      item = `<li class="win">W</li>`;
     } else if (i === 1 && comChoiceNum === 0) {
       console.log("이겼습니다.");
+      item = `<li class="win">W</li>`;
     } else if (i === 2 && comChoiceNum === 1) {
       console.log("이겼습니다.");
+      item = `<li class="win">W</li>`;
     } else {
       console.log("졌습니다.");
+      item = `<li class="lose">L</li>`;
     }
     setTimeout(restart, 1000);
+    //resultList.innerHTML = resultList.innerHTML + item;
+    resultList.innerHTML += item;
   });
 }
 
