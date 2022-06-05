696f519a5e8c5fe61420b3064769f2c3e5d883a8
jjang051 <flyjjang051@gmail.com>
2022-06-05 16:47:37 +0900

    fancy box 적용

--- pinterest/js/main.js
+++ pinterest/js/main.js
@@ -2,10 +2,11 @@ $.ajax({ url: "../data/typo.json" }).done(function (data) {
   const myWorks = data.typoList;
   let tempHtml = "";
   const imgFolder = data.imageFolder;
+  //prettier-ignore
   $.each(myWorks, function (idx, item) {
     console.log(item);
-    tempHtml += `<li class="item">
-    <a href="">
+    tempHtml += `<li class="item ${item.category}">
+    <a href="${imgFolder + item.img}" data-fancybox="${item.category}" data-caption="${item.title}">
       <div class="imgBox"><img src="${imgFolder + item.img}" alt="" /></div>
       <div class="infoBox">
         <h2>${item.title}</h2>
@@ -16,10 +17,28 @@ $.ajax({ url: "../data/typo.json" }).done(function (data) {
   </li>`;
   });
   $("#works .grid").html(tempHtml);
-  imagesLoaded(".grid", function () {
+  // const myLoaded = imagesLoaded(".grid", function () {
+  //   const itemList = new Isotope(".grid", {
+  //     itemSelector: ".item",
+  //     layoutMode: "masonry",
+  //   });
+  // });
+  // myLoaded.on("progress", function (instance, image) {
+  //   console.log(image.isLoaded);
+  // });
+  const myLoaded = imagesLoaded(".grid");
+  myLoaded.on("always", function () {
     const itemList = new Isotope(".grid", {
       itemSelector: ".item",
       layoutMode: "masonry",
+    });
+    $(".filter li").on("click", function (e) {
+      //e.preventDefault();
+      $(this).addClass("on").siblings().removeClass("on");
+      const filtering = $(this).data("filter-word");
+      itemList.arrange({ filter: `.${filtering}` });
+      return false;
+      //alert("aaaa");
     });
   });
 });
